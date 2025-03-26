50f20ea0ceca42abeab05b7db7f129d3df1a887f
Süleyman Denizhan <sdenizhan@windowslive.com>
2025-03-26 11:45:50 +0300

    fix: unknown

--- resources/views/themes/backend/default/inquiry/approved.blade.php
+++ resources/views/themes/backend/default/inquiry/approved.blade.php
@@ -105,27 +105,27 @@
 @push('scripts')
     <!-- third party js -->
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-buttons/js/buttons.flash.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-buttons/js/buttons.flash.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js') }}"></script>
     <script
-        src=" {{ asset('themes/backend/default/assets/libs/datatables.net-select/js/dataTables.select.min.js') }}"></script>
+        src="{{ asset('themes/backend/default/assets/libs/datatables.net-select/js/dataTables.select.min.js') }}"></script>
     <script src=" {{ asset('themes/backend/default/assets/libs/pdfmake/build/pdfmake.min.js') }}"></script>
     <script src=" {{ asset('themes/backend/default/assets/libs/pdfmake/build/vfs_fonts.js') }}"></script>
     <!-- third party js ends -->
@@ -217,7 +217,7 @@
                                             '<a href="'+ data.medical_form_link +'" class="dropdown-item copy_reference_link" data-id="'+ data.id +'">' +
                                             '<i class="fa fa-link"></i> {{ __('Copy Medical Form Link') }}' +
                                             '</a>';
-                                        }   
+                                        }
 
                                     @endrole
                                 }
@@ -274,7 +274,7 @@
                                     @can('edit-inquiry')
                                         html += '<a href="#" class="dropdown-item show_inquiry" data-id="'+ data.id +'"><i class="fas fa-eye"></i> {{ __('Show')  }}</a>';
                                     @endcan
-                                
+
                                     @can('view-inquiry')
                                         var viewURL = "{{ route('admin.inquiries.show', ':id') }}".replace(':id', data.id);
                                         html += '<a href="'+ viewURL +'" class="dropdown-item" data-id="'+ data.id +'"><i class="fas fa-mail-bulk"></i> {{ __('Show Inquiry') }}</a>';
