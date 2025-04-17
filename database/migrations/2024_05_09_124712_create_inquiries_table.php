6b526336929313ef49079ae4683547e48e350507
Süleyman Denizhan <sdenizhan@windowslive.com>
2025-04-17 12:58:43 +0300

    fix: sıkıldım artık hadi

--- database/migrations/2024_05_09_124712_create_inquiries_table.php
+++ database/migrations/2024_05_09_124712_create_inquiries_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('inquiries', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('treatment_id');
+            $table->integer('treatment_id');
             $table->integer('status')->default(\App\Enums\InquiryStatus::WAITING->value); //onaylandı, reddedildi, bekliyor,
             $table->integer('gender')->default(\App\Enums\Gender::None->value);
 
